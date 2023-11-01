f128104df2f34fe8b34d8aaf0fdb2249b513796f
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-11-01 23:25:33 +0100

    foreach added

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
 let word = 'texto';
-let wordArray = word.split('');
+let wordArray = word.toUpperCase().split('');
 console.log(wordArray);
 
 let actualRow = document.querySelector('.row')
 
-wordArray.forEach(() => {
-    actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`
+wordArray.forEach((item, index) => {
+    if(index === 0){
+        actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus">`
+    }else{
+        actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`
+    }
+    
 })
+
+let focusElement = document.querySelector(".focus");
+focusElement.focus();
 
 let squares = document.querySelectorAll('.square')
 squares = [...squares]
-console.log(squares)
+
+let userInput = []
+
+squares.forEach(element => {
+    element.addEventListener('input', event =>{
+        userInput.push(event.target.value.toUpperCase())
+        console.log(userInput)
+        if( event.target.nextElementSibling){
+            event.target.nextElementSibling.focus();
+        }else{
+           compareArrays(wordArray,userInput)
+        }
+       
+    })
+})
+
+// functions
+
+function compareArrays(array1, array2){
+    array1.forEach((element, index)=>{
+        if(element == array2[index]){
+            console.log(`en la posicion ${index} son iguales`);
+        }else{
+            console.log(`en la posicion ${index} no son iguales`);
+        }
+    })
+}
