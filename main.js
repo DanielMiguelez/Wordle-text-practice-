1c1cd47c6c7e290b5e191657495ad17b52363cdc
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-11-04 14:16:46 +0100

    focus added, squares created in actual row

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let resultElement = document.querySelector(".result");
+let mainContainer = document.querySelector(".main-container");
+let rowId = 1;
 
 let word = "texto";
 let wordArray = word.toUpperCase().split("");
@@ -6,48 +8,61 @@ console.log(wordArray);
 
 let actualRow = document.querySelector(".row");
 
-wordArray.forEach((item, index) => {
-  if (index === 0) {
-    actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus">`;
-  } else {
-    actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`;
-  }
-});
+drawSquares(actualRow);
+listenInput(actualRow);
+addFocus(actualRow);
 
-let focusElement = document.querySelector(".focus");
-focusElement.focus();
+function listenInput(actualRow) {
+  let squares = actualRow.querySelectorAll(".square");
+  squares = [...squares];
 
-let squares = document.querySelectorAll(".square");
-squares = [...squares];
+  let userInput = [];
 
-let userInput = [];
+  squares.forEach((element) => {
+    element.addEventListener("input", (event) => {
+      //recoger evento del user
+      userInput.push(event.target.value.toUpperCase());
+      console.log(userInput);
+      if (event.target.nextElementSibling) {
+        event.target.nextElementSibling.focus();
+      } else {
+        let rigthIndex = compareArrays(wordArray, userInput);
+        console.log(rigthIndex);
+        rigthIndex.forEach((element) => {
+          squares[element].classList.add("green");
+        });
 
-squares.forEach((element) => {
-  element.addEventListener("input", (event) => {
-    userInput.push(event.target.value.toUpperCase());
-    console.log(userInput);
-    if (event.target.nextElementSibling) {
-      event.target.nextElementSibling.focus();
-    } else {
-      let rigthIndex = compareArrays(wordArray, userInput);
-      console.log(rigthIndex);
-      rigthIndex.forEach((element) => {
-        squares[element].classList.add("green");
-      });
-      // compararemos array para cambiar los estilos
+        // compararemos array para cambiar los estilos
 
-      if (rigthIndex.length == wordArray.length) {
-        resultElement.innerHTML = `<p>Ganaste</p>
+        if (rigthIndex.length == wordArray.length) {
+          resultElement.innerHTML = `<p>Ganaste</p>
             <button class="button">Reiniciar</button>`;
-      }
 
-     /* let resetBtn = document.querySelector(".button");
-      resetBtn.addEventListener("click", () => {
-        location.reload();
-      });*/
-    }
+          let resetBtn = document.querySelector(".button");
+          resetBtn.addEventListener("click", () => {
+            location.reload();
+          });
+
+          return;
+        }
+
+        // creamos una nueva fila aqui abajo
+        let actualRow = createRow();
+        drawSquares(actualRow);
+        listenInput(actualRow);
+        addFocus(actualRow);
+
+        // cambiar estilos si existe letra pero no esta en poaicion correcta
+
+        let existIndexArray = existLetter(wordArray, userInput);
+
+        existIndexArray.forEach((element) => {
+          squares[element].classList.add("gold");
+        });
+      }
+    });
   });
-});
+}
 
 // functions
 
@@ -62,4 +77,39 @@ function compareArrays(array1, array2) {
     }
   });
   return equalsIndex;
+}
+
+function existLetter(array1, array2) {
+  let existIndexArray = [];
+  array2.forEach((element, index) => {
+    if (array1.includes(element)) {
+      existIndexArray.push(index);
+    }
+  });
+  return existIndexArray;
+}
+
+function createRow() {
+  rowId++;
+  let newRow = document.createElement("div");
+  newRow.classList.add("row");
+  newRow.setAttribute("id", rowId);
+  mainContainer.appendChild(newRow);
+  return newRow;
+}
+
+function drawSquares(actualRow) {
+  wordArray.forEach((item, index) => {
+    if (index === 0) {
+      actualRow.innerHTML += `<input type="text" maxlength="1" class="square focus">`;
+    } else {
+      actualRow.innerHTML += `<input type="text" maxlength="1" class="square">`;
+    }
+  });
+}
+
+function addFocus(actualRow) {
+  let focusElement = actualRow.querySelector(".focus");
+  console.log(focusElement);
+  focusElement.focus();
 }
